00bdbc9a302396ef76cd1600525093471596383b
OubriDaouda <oubridaouda@gmail.com>
2022-03-29 01:50:35 +0100

    feat: add login page

---
+++ view/LoginView.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" type="text/css" media="screen" href="sass/custom/login.css"/>
+<div class="login-page">
+    <div class="login-card">
+        <form method="POST" action="">
+            <input type="hidden" name="action" value="create"/>
+            <div class="username-input">
+                <input type="text" name="action" value="create"/>
+                <label for="firstname">Nom d'utilisateur</label>
+            </div>
+            <div class="user-password">
+                <input type="password" name="action" value="create"/>
+                <label for="firstname">Mot de pass</label>
+            </div>
+        </form>
+    </div>
+</div>
