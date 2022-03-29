8a203120f3fd1bc5e4ffa3090cb9eacebef712ce
OubriDaouda <oubridaouda@gmail.com>
2022-03-29 17:20:47 +0100

    feat: page router foreach loop page and url matched page

--- routes/Route.php
+++ routes/Route.php
@@ -6,7 +6,7 @@ class Route
 {
 
     public $path;
-    public $action ;
+    public $action;
     public $matches;
 
     public function __construct($path, $action)
@@ -15,32 +15,32 @@ class Route
         $this->action = $action;
     }
 
+    /**
+     * Permettra de recupere l'url avec ses paramètre
+     * get('/posts/:id') par exemple
+     **/
     public function matches(string $url)
     {
-        //conversion de l'url avec des expressions regulieres
+        $path = preg_replace('#:([\w]+)#', '([^/]+)', $this->path); //conversion de l'url avec des expressions regulieres
+        $pathToMatch = "#^$path$#i"; //expressions regulieres permettant de s'assurer l'unicité de l'url
 
-        $path = preg_replace('#:([\w]+)#', '[^/]+', $this->path);
-        $pathToMatch = "#$path$#";
-
-        if (preg_match($pathToMatch, $url, $matches)){
+        if (preg_match($pathToMatch, $url, $matches)) {
             $this->matches = $matches;
-            var_dump($this->matches);
+//            var_dump($this->matches);
 
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public function execute(string $url){
-        //recupere l'action le sépare en controller et methode
-        $params = explode('@', $this->action);
+    public function execute(string $url)
+    {
+        $params = explode('@', $this->action); //recupere l'action le sépare en controller et methode
         $urlParser = explode('/', $url);
         $controller = new $params[0]();
         $method = $params[1];
-//
-//        var_dump($urlParser[1]);
+//      var_dump($urlParser[1]);
         return isset($urlParser[1]) ? $controller->$method($urlParser[1]) : $controller->$method();
     }
 }
--- routes/Router.php
+++ routes/Router.php
@@ -1,34 +1,39 @@
 <?php
 /* Ici sera effectuer tout le routage du site web */
+
 namespace Router;
 
-class Router{
+class Router
+{
 
     public $url;
     public $routes = [];
 
     //L'$url que l'on recupere avec construct
-    public function __construct($url){
+    public function __construct($url)
+    {
 
         $this->url = trim($url, '/');
     }
 
     //la function de routage
-    public function get(string $path, string $action){
+    public function get(string $path, string $action)
+    {
         //Les routes sont stocké dans un tableau
         $this->routes['GET'][] = new Route($path, $action);
     }
 
-    public function run(){
-        //bouclage sur les routes
-        foreach($this->routes[$_SERVER['REQUEST_METHOD']] as $route){
-            //verifie si l'url existe
-            if($route ->matches($this->url)){
-                var_dump($route->matches($this->url));
-                $route ->execute($this->url);
-            };
+    //Verifie si la route existe et retourne la vue correspondante
+    public function run()
+    {
+        if (!isset($this->routes[$_SERVER['REQUEST_METHOD']])) {
+            return header("HTTP/1.0 404 Not Found");
         }
-        //si non return http not found
-        return header ("HTTP/1.0 404 Not Found");
+        foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $route) {
+            if ($route->matches($this->url)) {
+                $route->execute($this->url);
+            }
+        }
+        return header("HTTP/1.0 404 Not Found");
     }
 }
