f753798890977f52dc6cb4c4cf55b12c14f7277c
OubriDaouda <oubridaouda@gmail.com>
2022-03-30 13:45:01 +0100

    feat: add product card

--- view/product/index.php
+++ view/product/index.php
@@ -1,14 +1,104 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 
-<h1> imply random text. It has roots in a piece of classical Latin literature from 4</h1>
-</body>
-</html>
+<div class="shell">
+    <div class="products-container">
+        <div class="products-card">
+            <div class="card">
+                <div class="wsk-cp-product">
+                    <div class="wsk-cp-img">
+                        <img src="https://3.bp.blogspot.com/-eDeTttUjHxI/WVSvmI-552I/AAAAAAAAAKw/0T3LN6jABKMyEkTRUUQMFxpe6PLvtcMMwCPcBGAYYCw/s1600/001-culture-clash-matthew-gianoulis.jpg"
+                             alt="Product" class="img-responsive"/>
+                    </div>
+                    <div class="wsk-cp-text">
+                        <div class="category">
+                            <span>Ethnic</span>
+                        </div>
+                        <div class="title-product">
+                            <h3>My face not my heart</h3>
+                        </div>
+                        <div class="description-prod">
+                            <p>Description Product tell me how to change playlist height size like 600px in html5
+                                player. player good work now check this link</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="wcf-left"><span class="price">Rp500.000</span></div>
+                            <div class="wcf-right"><a href="#" class="buy-btn"><i class="zmdi zmdi-shopping-basket"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="wsk-cp-product">
+                    <div class="wsk-cp-img"><img
+                                src="https://1.bp.blogspot.com/-b-2SgNUrFHg/WVSvmewWqgI/AAAAAAAAAK0/1K4YCcbYjhokHwV_IgiVJN9mEnQoWunIwCPcBGAYYCw/s1600/fashion-portrait-nicole-6347.jpg"
+                                alt="Product" class="img-responsive"/></div>
+                    <div class="wsk-cp-text">
+                        <div class="category">
+                            <span>Introvert</span>
+                        </div>
+                        <div class="title-product">
+                            <h3>My face not my heart</h3>
+                        </div>
+                        <div class="description-prod">
+                            <p>Description Product tell me how to change playlist height size like 600px in html5
+                                player. player good work now check this link</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="wcf-left"><span class="price">Rp500.000</span></div>
+                            <div class="wcf-right"><a href="#" class="buy-btn"><i class="zmdi zmdi-shopping-basket"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="wsk-cp-product">
+                    <div class="wsk-cp-img"><img
+                                src="https://1.bp.blogspot.com/-XL_Ba-178Fo/WVSvm5AbApI/AAAAAAAAAK4/X5109HTqUiAhPjbmz4NFVHcpL7ZWe6T3ACPcBGAYYCw/s1600/wow-29.jpg"
+                                alt="Product" class="img-responsive"/></div>
+                    <div class="wsk-cp-text">
+                        <div class="category">
+                            <span>Beauty</span>
+                        </div>
+                        <div class="title-product">
+                            <h3>My face not my heart</h3>
+                        </div>
+                        <div class="description-prod">
+                            <p>Description Product tell me how to change playlist height size like 600px in html5
+                                player. player good work now check this link</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="wcf-left"><span class="price">Rp500.000</span></div>
+                            <div class="wcf-right"><a href="#" class="buy-btn"><i class="zmdi zmdi-shopping-basket"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="wsk-cp-product">
+                    <div class="wsk-cp-img"><img
+                                src="https://3.bp.blogspot.com/-iUes8qr4uC8/WVSvl6Wf_fI/AAAAAAAAAKs/JiTUUVvdbqEC_QGGaQhuVJiezIN3LIqEgCPcBGAYYCw/s1600/IMG_5367-bp.jpg"
+                                alt="Product" class="img-responsive"/></div>
+                    <div class="wsk-cp-text">
+                        <div class="category">
+                            <span>Drama</span>
+                        </div>
+                        <div class="title-product">
+                            <h3>My face not my heart cvf ggf gfg g</h3>
+                        </div>
+                        <div class="description-prod">
+                            <p>Description Product tell me how to change playlist height size like 600px in html5
+                                player. player good work now check this link</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="wcf-left"><span class="price">Rp500.000</span></div>
+                            <div class="wcf-right"><a href="#" class="buy-btn"><i class="zmdi zmdi-shopping-basket"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
