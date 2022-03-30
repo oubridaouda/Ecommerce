7217f49409921e50c2ef731a9117cad7fd40812a
OubriDaouda <oubridaouda@gmail.com>
2022-03-30 13:46:52 +0100

    feat: add new navbar

--- view/layout.php
+++ view/layout.php
@@ -6,12 +6,70 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="sass/custom/login.css"/>
+    <link rel="stylesheet" href="
+    <?= SCRIPT . 'assets' . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . 'card.css?=' . time() ?>"/>
+    <link rel="stylesheet"
+          href="<?= SCRIPT . 'assets' . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . 'navbar.css?=' . time() ?>"/>
 
 </head>
 <body>
 
+<nav>
+    <input type="checkbox" id="check"/>
+    <label for="check" class="menu">
+    </label>
+    <div class="logo">
+        <h2>NavBar</h2>
+    </div>
+    <div class="nav-item-content">
+
+        <div class="nav-items">
+            <ul class="overview">
+                <h3>Overview</h3>
+                <li>
+                    <a href="/Ecommerce/"
+                    >
+                        Home</a
+                    >
+                </li>
+                <li>
+                    <a href="/Ecommerce/products">
+                        Produits</a
+                    >
+                </li>
+                <li>
+                    <a href="/login"
+                    >
+                        Rates</a
+                    >
+                </li>
+                <li>
+                    <a href="/signup"
+                    >
+                        Contact</a
+                    >
+                </li>
+            </ul>
+            <ul class="account">
+                <h3>Account</h3>
+                <li>
+                    <a href="/Ecommerce/login"
+                    >
+                        Se connecter</a
+                    >
+                </li>
+                <li>
+                    <a href="/Ecommerce/signup"
+                    >
+                        S'inscrir</a
+                    >
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <!--        Toutes les page sont appelé dans le container via $content -->
-    <div class="container"><?= $content ?></div>
+<div class="container"><?= $content ?></div>
 </body>
 </html>
---
+++ view/navbar.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Nav Bar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <!-- <script src="main.js"></script> -->
+  </head>
+  <body>
+    <nav>
+      <input type="checkbox" id="check" />
+      <label for="check" class="menu">
+      </label>
+      <div class="logo">
+        <h2>NavBar</h2>
+      </div>
+      <div class="nav-items">
+        <ul class="overview">
+          <h3>Overview</h3>
+          <li>
+            <a href="#"
+               >
+              Home</a
+            >
+          </li>
+          <li>
+            <a href="#">
+              Dashboard</a
+            >
+          </li>
+          <li>
+            <a href="#"
+               >
+              Rates</a
+            >
+          </li>
+          <li>
+            <a href="#"
+               >
+              Contact</a
+            >
+          </li>
+        </ul>
+        <ul class="account">
+          <h3>Account</h3>
+          <li>
+            <a href="#"
+               >
+              Notifications</a
+            >
+          </li>
+          <li>
+            <a href="#"
+               >
+              Settings</a
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="hero-section"></div>
+  </body>
+</html>
