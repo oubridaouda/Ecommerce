6776768639117e1aab57af364d12ad5906f04c18
OubriDaouda <oubridaouda@gmail.com>
2022-04-03 00:05:36 +0100

    fix: style to scss

--- view/connexion/Login.php
+++ view/connexion/Login.php
@@ -1,22 +1,12 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<html>
-<head>
-    <title>Login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <link rel="stylesheet"
-          href="<?= SCRIPT . 'assets' . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . 'login_style.css' ?>"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
-    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
-          integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-</head>
-
-<body class="body">
-
+<?php
+$message = false;
+$succes = -1;
+if (isset($_GET['succes'])) {
+    $succes = $_GET['succes'];
+    $message = ($succes) ? "Connexion reussie" : "Echec: Informations incorrectes";
+    $class = ($succes) ? "btn btn-success" : "btn btn-danger";
+}
+?>
 <div class="login-page">
     <div class="form">
         <form action="/login" method="POST">
@@ -48,6 +38,3 @@
         }
     };
 </script>
-</body>
-</html>
-
--- view/connexion/SignUp.php
+++ view/connexion/SignUp.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta charset="utf-8"/>
     <link rel="stylesheet"
-          href="<?= SCRIPT . 'assets' . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . 'signup_style.css' ?>"/>
+          href="<?= SCRIPT . 'assets' . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . 'signup_style.scss' ?>"/>
     <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
